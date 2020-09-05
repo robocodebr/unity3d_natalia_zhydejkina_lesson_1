8d7c1c7d096eb85408f1a061f89ae32a3a8e382e
Karina Varlena <karina.varlena@instantconnectnow.com>
2020-09-05 15:47:56 +0300

    first lesson"

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x= 0;
+            int y= 0;
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
